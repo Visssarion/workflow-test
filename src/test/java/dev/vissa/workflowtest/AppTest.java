7562522ec84b38c65a6ff89cacd3f198d67b9e12
Vissa <46628714+Visssarion@users.noreply.github.com>
2024-11-30 15:14:05 +0300

    Delete Maven from project

--- src/test/java/dev/vissa/workflowtest/AppTest.java
+++ src/test/java/dev/vissa/workflowtest/AppTest.java
@@ -1,13 +1,9 @@
 package dev.vissa.workflowtest;
 
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import org.junit.jupiter.api.Test;
 
 public class AppTest {
-	@Test
 	public void test() {
-		assertEquals(true, true);
+		assert(true);
 	}
 }
