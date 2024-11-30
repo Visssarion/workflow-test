887c04b37dd9f728630e9dea2584b6cb52b6a7a5
Vissa <46628714+Visssarion@users.noreply.github.com>
2024-11-30 14:53:21 +0300

    Init Commit

---
+++ src/main/java/dev/vissa/workflowtest/App.java
@@ -0,0 +1,10 @@
+package dev.vissa.workflowtest;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
---
+++ src/test/java/dev/vissa/workflowtest/AppTest.java
@@ -0,0 +1,13 @@
+package dev.vissa.workflowtest;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+	@Test
+	public void test() {
+		assertEquals(true, true);
+	}
+}
